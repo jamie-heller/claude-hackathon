504573625020892d671a39b73b4125588a83d3f0
Jamie Heller <jheller@citrine.io>
2023-07-29 15:11:15 -0700

    Refactor main flow into a function -- use it to iterate over test_corpus

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 from anthropic import Anthropic, HUMAN_PROMPT, AI_PROMPT
+import pydantic_core
 from PyPDF2 import PdfReader
 import os
 import sys
 import json
 from pydantic.dataclasses import dataclass
 
+anthropic = Anthropic(api_key=os.environ.get("ANTHROPIC_API_KEY"))
 
-example_pdf = "/Users/jamieheller/code/workspace/CitrineInformatics/claude-hackathon/corpus/#2-Corrosion Science-1999--Electrochemical investigation of the influence of nitrogen alloying on pitting corrosion of austenitic stainless steels.pdf"
-reader = PdfReader(example_pdf)
-number_of_pages = len(reader.pages)
-page = reader.pages[0]
-pdf_content = ""
-for page in reader.pages:
-    text = page.extract_text()
-    pdf_content = pdf_content + "\n" + text
+
+@dataclass
+class StructuredOutput:
+    file_name: str
+    processes: list[str]
+    features: list[str]
+    properties: list[str]
 
 
 def eprintln(arg):
@@ -34,70 +35,88 @@ def parse_output(claude_output: str) -> str:
     return claude_output[0:end_index]
 
 
-my_api_key = os.environ.get("ANTHROPIC_API_KEY")
-anthropic = Anthropic(api_key=my_api_key)
-my_prompt = (
-    f"{HUMAN_PROMPT} "
-    "Please help me summarize, in a structured way,  "
-    "the following academic paper related to manufacturing steel "
-    "and its resultant properties."
-    "The document be provided within <document></document> tags. "
-    "In particular, you will be taking the following steps, that result in an output "
-    "json object, included within <output></output> tags. "
-    "1. You will list all of the manufacturing and processing steps for steel. "
-    '  Please include each individual step within a json array of strings in the the "processes" field '
-    "of the output."
-    "2. List at least 5 microstructural features which determine the performance of steel"
-    '  Please include each individual feature within a json array of strings in the "features" field '
-    "of the output object."
-    "3. List at least five properties essential for a high performance steel. "
-    '  Please include each individual property within a json array of strings in the "properties" field '
-    "of the output object."
-    "Please be sure to include the overall JSON output within <output></output> tags. "
-    "Example output is included in the following <example></example> tags. "
-    "<example>"
-    "<output>"
-    "{"
-    '  "processes": ["Process 0", "Process 1", "Process 2"]'
-    '  "features": ["Feature 0", "Feature 1", "Feature 2"]'
-    '  "properties": ["Property 0", "Property 1", "Property 2"]'
-    "}"
-    "</output>"
-    "</example>"
-    "Please ONLY output the JSON object within the <output></output> tags and nothing else"
-    f"<document>{pdf_content}</document>"
-    f"{AI_PROMPT} <output>"
-)
-stream = anthropic.completions.create(
-    model="claude-2",
-    max_tokens_to_sample=1000,
-    prompt=my_prompt,
-    stream=True,
-)
-raw_output = ""
-for completion in stream:
-    s = completion.completion
-    eprint(s)
-    raw_output = raw_output + s
-
-eprintln("")
-
-# eprintln(raw_output)
-raw_json_str = parse_output(raw_output)
-print(raw_json_str)
-output = json.loads(raw_json_str)
-
-# eprintln(output)
+def get_pdf_content(pdf_path: str) -> str:
+    reader = PdfReader(pdf_path)
+    pdf_content = ""
+    for page in reader.pages:
+        text = page.extract_text()
+        pdf_content = pdf_content + "\n" + text
+    return pdf_content
 
 
-@dataclass
-class Output:
-    processes: list[str]
-    features: list[str]
-    properties: list[str]
+def build_prompt(topic: str, pdf_content: str) -> str:
+    return (
+        f"{HUMAN_PROMPT} "
+        f"The following is an academic paper in the materials science field related "
+        "to manufacturing and designing {topic} "
+        "in order to affect certain properties of the material."
+        "The document be provided within <document></document> tags. "
+        "Here is the document:"
+        f"<document>{pdf_content}</document>"
+        "Please get some structured data from that document."
+        "In particular, you will be taking the following steps, that result in an "
+        "output json object, included within <output></output> tags. "
+        f"1. You will list all of the manufacturing and processing steps for {topic}. "
+        "  Please include each individual step within a json array of strings in the "
+        '"processes" field '
+        "of the output."
+        "2. List at least 5 microstructural features which determine the performance of"
+        f"of {topic}"
+        "  Please include each individual feature within a json array of strings in "
+        'the "features" field '
+        "of the output object."
+        "3. List at least five properties essential for a high performance steel. "
+        "  Please include each individual property within a json array of strings in "
+        'the "properties" field "of the output object.'
+        "Please be sure to include the overall JSON output within <output></output> "
+        "tags. Example output is included in the following <example></example> tags. "
+        "<example>"
+        "<output>"
+        "{"
+        '  "processes": ["Process 0", "Process 1", "Process 2"]'
+        '  "features": ["Feature 0", "Feature 1", "Feature 2"]'
+        '  "properties": ["Property 0", "Property 1", "Property 2"]'
+        "}"
+        "</output>"
+        "</example>"
+        "Please ONLY output the JSON object within the <output></output> tags and "
+        "nothing else."
+        f"<document>{pdf_content}</document>"
+        f"{AI_PROMPT} <output>"
+    )
 
 
-typed_output = Output(**output)
+def process_document(document_path: str) -> StructuredOutput:
+    pdf_content = get_pdf_content(document_path)
+    my_prompt = build_prompt("steel", pdf_content)
+    stream = anthropic.completions.create(
+        model="claude-2",
+        max_tokens_to_sample=1000,
+        prompt=my_prompt,
+        stream=True,
+    )
+    raw_output = ""
+    for completion in stream:
+        s = completion.completion
+        eprint(s)
+        raw_output = raw_output + s
+    eprintln("")
+    raw_json_str = parse_output(raw_output)
+    json_output = json.loads(raw_json_str)
+    json_output["file_name"] = document_path
+    typed_output = StructuredOutput(**json_output)
+    return typed_output
 
 
-# eprintln(typed_output)
+CORPUS_DIR = "./test_corpus"
+files_to_process = os.listdir(CORPUS_DIR)
+for file in files_to_process[:2]:
+    results: list[StructuredOutput] = []
+    eprintln(f"Processing document: {file}")
+    output = process_document(f"{CORPUS_DIR}/{file}")
+    eprintln("Result is: ")
+    results.append(output)
+    json_results = []
+    for r in results:
+        json_results.append(pydantic_core.to_jsonable_python(r))
+    eprintln(json.dumps(json_results, indent=2))
